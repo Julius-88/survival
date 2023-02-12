5a0215b1d6b44debc1344a7e254a3efc11659918
Julius Alamarvdashti <drako886@gmail.com>
2023-02-12 13:56:00 +0000

    Add function to stay in apartment

--- run.py
+++ run.py
@@ -6,14 +6,15 @@ This is a survival game
 def choice():
     """
     Presents user with their first choice of leaving the apartment or staying.
-    If they press a they will leave apartment, if they press b they will stay.
+    If they press "a" they will leave apartment, if they press "b"
+    they will stay.
     """
-    action = input('Press "a" to leave your apartment, or "b" to stay: ')
+    action = input('Press "a" to leave your apartment or "b" to stay: ')
     if action != ' ':
         if action == 'a':
             choice_a()
         elif action == 'b':
-            print('b')
+            choice_b()
         else:
             print('invalid choice, try again')
             choice()
@@ -24,10 +25,13 @@ def choice():
 
 def choice_a():
     """
-    If the user pressed "a" on choice they will be brought here.
+    If the user pressed "a" to leave apartment on choice they will be brought
+    here.
     """
-    print('You have chosen to leave your apartment.')
-    print('Will you look for a weapon before you leave?')
+    print('Filled with adrenaline you have chosen to leave your apartment. '
+          'You suddenly realize it might be a good idea to have some '
+          'protection. Will you look for a weapon before you leave?')
+
     action = input('Press "a" to look for a weapon, or "b" to leave ')
     if action != ' ':
         if action == 'a':
@@ -42,6 +46,33 @@ def choice_a():
         choice_a()
 
 
+def choice_b():
+    """
+    If the user pressed "b" to stay on choice they will be brought here.
+    """
+    print('You have chosen to stay in your apartment. '
+          'Congratulations you survived the first night! '
+          'You have consumed some of your resources. '
+          'You wake up the next day in a haze, a part of you does not want to '
+          'believe what has happened. You look around the apartment '
+          'and try to decide what to do next. '
+          'You can turn on the tv to see the news '
+          'or you could look at your phone to see if anyone has called you.')
+
+    action = input('Press "a" to turn on the tv or "b" to look at your phone ')
+    if action != ' ':
+        if action == 'a':
+            print('tv')
+        elif action == 'b':
+            print('phone')
+        else:
+            print('invalid choice, try again')
+            choice_b()
+    else:
+        print('invalid choice, try again')
+        choice_b()
+
+
 def name():
     """
     checks to see if player has entered a name
@@ -52,8 +83,9 @@ def name():
         name()
     else:
         print(f'Welcome {player_name}, ')
-        print('You have been trapped in your apartment!')
-        print('Zombies are attacking your city. What do you do? ')
+        print('You have been trapped in your apartment! '
+              'Zombies are attacking your city. '
+              'What do you do? ')
 
 
 name()
